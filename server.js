9ac41682066ac29aab10858ae7909ff66496c231
SobolRR <sobolroman22@gmail.com>
2016-11-11 18:14:57 +0200

    stoped on Ответ обработчиков запроса с неблокирующими операциями.

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var http = require("http");
 var url = require("url");
 
 
-function start(route) {
+function start(route, handle) {
     function onRequest(request, response) {
         console.log("Request received.");
         var pathname = url.parse(request.url).pathname;
-        route(pathname);
+        route(pathname, handle);
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World");
         response.end();
